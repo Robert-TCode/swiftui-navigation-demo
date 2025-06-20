be76d9669ce41357a51cd70ae485fcaef7bc5003
Robert Tanase <robert.tanase97@gmail.com>
2025-06-20 12:27:13 +0300

    Initial Commit

---
+++ ModularNavigationApp/ModularNavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularNavigationAppApp.swift
+//  ModularNavigationApp
+//
+//  Created by TCode on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ModularNavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModularNavigationAppTests/ModularNavigationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModularNavigationAppTests.swift
+//  ModularNavigationAppTests
+//
+//  Created by TCode on 20/6/25.
+//
+
+import Testing
+@testable import ModularNavigationApp
+
+struct ModularNavigationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
